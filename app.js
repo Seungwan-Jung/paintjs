f6a2540f0f504546767f24f6249deb44ed7411fd
Seungwan Jung <antitheseaboutcommonsense@gmail.com>
2021-05-13 02:31:29 +0900

    2.5 filling mode

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2C2C2C";
+const CANVAS_SIZE=  700;
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
 let painting = false;
 let filling = false;
@@ -39,6 +42,7 @@ function onMouseMove(event) {
 function handleColorClick(event){
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event){
@@ -56,11 +60,17 @@ function handleModeClick(){
   }
 }
 
+function handleCanvasClick(){
+  if(filling){
+    ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+  }
+}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click",handleCanvasClick);
 }
 
 Array.from(colors).forEach(color =>
